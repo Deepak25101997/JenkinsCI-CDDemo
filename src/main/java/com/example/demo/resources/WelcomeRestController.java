d8019e106d62fae52ed9b8a511e9c55b46351dcc
Deepak Kumar <deepak.k25101997@gmail.com>
2020-10-27 20:19:18 +0530

    Code updated

--- src/main/java/com/example/demo/resources/WelcomeRestController.java
+++ src/main/java/com/example/demo/resources/WelcomeRestController.java
@@ -17,7 +17,7 @@ public class WelcomeRestController {
 		return "Welcome to Sporty Shoes !" + name;
 	}
 		
-	@GetMapping("/admin")
+	@GetMapping("/admins")
 	public String returnAdmin() {
 		return "Name: Deepak. Contact: deepak.kumar@prolim.com" ;
 	}
